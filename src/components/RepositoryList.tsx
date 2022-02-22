94298caa61354159783c74693c7b6bfa98650818
Arthur Moreira de Deus <arthur.md@parade.ai>
2022-02-22 13:55:13 -0300

    feat: Type components

--- src/components/RepositoryList.tsx
+++ src/components/RepositoryList.tsx
@@ -3,6 +3,12 @@ import { RepositoryItem } from './RepositoryItem'
 
 import '../styles/repositories.scss'
 
+interface Repository {
+  name: string;
+  description: string;
+  html_url: string;
+}
+
 const repository = {
   name: 'unform',
   description: 'Forms in React',
@@ -10,7 +16,7 @@ const repository = {
 }
 
 export function RepositoryList() {
-  const [repositories, setRepositories] = useState([])
+  const [repositories, setRepositories] = useState<Repository[]>([])
 
   useEffect(() => {
     fetch('https://api.github.com/orgs/rocketseat/repos')
