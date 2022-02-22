9a247972d8e369c36cc76726c609b6e200c48937
Arthur Moreira de Deus <54866778+arthurdedeus@users.noreply.github.com>
2022-02-22 13:56:54 -0300

    feat: Using typescript (#6)
    * feat: Configure typescript
    
    * feat: Type components

--- src/components/RepositoryList.tsx
+++ src/components/RepositoryList.tsx
@@ -1,32 +1,37 @@
 import { useState, useEffect } from 'react'
-import { RepositoryItem } from "./RepositoryItem";
+import { RepositoryItem } from './RepositoryItem'
 
 import '../styles/repositories.scss'
 
+interface Repository {
+  name: string;
+  description: string;
+  html_url: string;
+}
+
 const repository = {
-  name: "unform",
-  description: "Forms in React",
-  link: "https://github.com/unform/unform"
+  name: 'unform',
+  description: 'Forms in React',
+  link: 'https://github.com/unform/unform',
 }
 
 export function RepositoryList() {
-  const [repositories, setRepositories] = useState([])
+  const [repositories, setRepositories] = useState<Repository[]>([])
 
   useEffect(() => {
     fetch('https://api.github.com/orgs/rocketseat/repos')
-      .then(response => response.json())
-      .then(data => setRepositories(data))
+      .then((response) => response.json())
+      .then((data) => setRepositories(data))
   }, [])
 
-  return(
+  return (
     <section className="repository-list">
       <h1>Lista de repositórios</h1>
 
       <ul>
-        {repositories.map(repository => {
+        {repositories.map((repository) => {
           return <RepositoryItem key={repository.name} repository={repository} />
         })}
-
       </ul>
     </section>
   )
