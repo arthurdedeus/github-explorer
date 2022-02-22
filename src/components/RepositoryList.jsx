0411371d6c70258b364ed55fbbb5794ea70262f1
Arthur Moreira de Deus <arthur.md@parade.ai>
2022-02-22 13:36:00 -0300

    feat: Configure typescript

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,12 +1,12 @@
 import { useState, useEffect } from 'react'
-import { RepositoryItem } from "./RepositoryItem";
+import { RepositoryItem } from './RepositoryItem'
 
 import '../styles/repositories.scss'
 
 const repository = {
-  name: "unform",
-  description: "Forms in React",
-  link: "https://github.com/unform/unform"
+  name: 'unform',
+  description: 'Forms in React',
+  link: 'https://github.com/unform/unform',
 }
 
 export function RepositoryList() {
@@ -14,19 +14,18 @@ export function RepositoryList() {
 
   useEffect(() => {
     fetch('https://api.github.com/orgs/rocketseat/repos')
-      .then(response => response.json())
-      .then(data => setRepositories(data))
+      .then((response) => response.json())
+      .then((data) => setRepositories(data))
   }, [])
 
-  return(
+  return (
     <section className="repository-list">
       <h1>Lista de repositórios</h1>
 
       <ul>
-        {repositories.map(repository => {
+        {repositories.map((repository) => {
           return <RepositoryItem key={repository.name} repository={repository} />
         })}
-
       </ul>
     </section>
   )
